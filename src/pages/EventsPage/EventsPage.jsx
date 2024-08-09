b5b1a57fe395cff7daa2ebbfdab5476660835e98
tstidwill <tara.stidwill@gmail.com>
2024-08-09 13:02:10 -0400

    Add more responsive design to Events page

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -8,6 +8,9 @@ import waterImage from "../../assets/images/pexels-danielspase-2091351.jpg";
 import treeImage from "../../assets/images/pexels-mikegreer-photos-1390371.jpg";
 import conferenceImage from "../../assets/images/pexels-wildlittlethingsphoto-705792.jpg";
 import MobileFooter from "../../components/MobileFooter/MobileFooter";
+import Header from "../../components/Header/Header";
+import LandingFooter from "../../components/LandingFooter/LandingFooter";
+import Footer from "../../components/Footer/Footer";
 
 export default function LandingPage() {
   const API_URL = import.meta.env.VITE_CORS_ORIGIN;
@@ -30,62 +33,65 @@ export default function LandingPage() {
 
   return (
     <>
-      <div className="logobox">
+      {/* <div className="logobox">
         <img src={greenLogo} className="logo" alt="leaf logo"></img>
-      </div>
+      </div> */}
+      <Header />
       <section className="events">
         <section className="events__headerbox">
           <h3>Upcoming Events</h3>
         </section>
-
-        {error && <p>{error} </p>}
-        {events &&
-          events.map((event) => {
-            return (
-              <section className="events__card card" key={event.id}>
-                {event.type === "conference" && (
-                  <img
-                    src={conferenceImage}
-                    alt="Conference event"
-                    className="card__image"
-                  />
-                )}
-                {event.type === "park_cleanup" && (
-                  <img
-                    src={pickupImage}
-                    alt="Cleaning up park"
-                    className="card__image"
-                  />
-                )}
-                {event.type === "tree_planting" && (
-                  <img
-                    src={treeImage}
-                    alt="Seed of a plant"
-                    className="card__image"
-                  />
-                )}
-                {event.type === "water_cleanup" && (
-                  <img src={waterImage} alt="Lake" className="card__image" />
-                )}
-                <h4 className="card__title"> {event.name}</h4>
-                <div className="card__details">
-                  <h5>{event.date}</h5>
-                  <h5>
-                    {event.location}, {event.city}
-                  </h5>
-                  <a
-                    href={event.link}
-                    className="button--square events__button"
-                    target="_blank"
-                  >
-                    Find out more
-                  </a>
-                </div>
-              </section>
-            );
-          })}
+        <section className="events__cards">
+          {error && <p>{error} </p>}
+          {events &&
+            events.map((event) => {
+              return (
+                <section className="events__card card" key={event.id}>
+                  {event.type === "conference" && (
+                    <img
+                      src={conferenceImage}
+                      alt="Conference event"
+                      className="card__image"
+                    />
+                  )}
+                  {event.type === "park_cleanup" && (
+                    <img
+                      src={pickupImage}
+                      alt="Cleaning up park"
+                      className="card__image"
+                    />
+                  )}
+                  {event.type === "tree_planting" && (
+                    <img
+                      src={treeImage}
+                      alt="Seed of a plant"
+                      className="card__image"
+                    />
+                  )}
+                  {event.type === "water_cleanup" && (
+                    <img src={waterImage} alt="Lake" className="card__image" />
+                  )}
+                  <h4 className="card__title"> {event.name}</h4>
+                  <div className="card__details">
+                    <h5>{event.date}</h5>
+                    <h5>
+                      {event.location}, {event.city}
+                    </h5>
+                    <a
+                      href={event.link}
+                      className="button--square events__button"
+                      target="_blank"
+                    >
+                      Find out more
+                    </a>
+                  </div>
+                </section>
+              );
+            })}
+        </section>
       </section>
       <MobileFooter />
+      <Footer />
     </>
   );
 }
